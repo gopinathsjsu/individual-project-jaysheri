a31af94617a94a46f8ddf36787e2a013d0befa67
jayanthreddysheri <123jayanth123@gmail.com>
2022-05-04 14:28:29 -0700

    Cards model creation

---
+++ src/model/Cards.java
@@ -0,0 +1,16 @@
+package model;
+
+public class Cards {
+    private String cardNumber;
+
+    public Cards(String card){
+        this.cardNumber = card;
+    }
+    public void setCardNumbers(String cardNumber) {
+        this.cardNumber = cardNumber;
+    }
+    public String getCardNumbers() {
+        return cardNumber;
+    }
+
+}
